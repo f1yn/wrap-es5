c29d1fbe9d8abd824d637c233e8d9057c4e2b980
flynnham <dev@flynnbuckingham.com>
2017-08-05 17:09:50 -0400

    Added filepath support

---
+++ index.js
@@ -0,0 +1,5 @@
+// this is a wrapper that allows fot the export of the filepath of the file for building purposes
+
+var path = require('path');
+
+module.exports = path.join(__dirname, './module.js');
